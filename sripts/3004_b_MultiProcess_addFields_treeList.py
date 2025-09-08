b0d86307431f5c3a98ac47ae71ef19b280b6d886
awuah-kwame <kawuah@forsite.ca>
2025-09-07 21:02:47 -0700

    refactor to lates script

--- sripts/3004_b_MultiProcess_addFields_treeList.py
+++ sripts/3004_b_MultiProcess_addFields_treeList.py
@@ -4,16 +4,16 @@ import time
 from shared.trees_to_csv_sp_split_ami import *
 from multiprocessing import Pool
 
-# #Adding fields to the hexagon feature classes to prepare for summarized data
-# Start = time.time()
-# # define working directory
-# config = read_yaml_config()
-# hex_root = config['root_folder']
-# hex_output_folder = config['hex_output_folder']
-# hex_gdb = config['hex_gdb']
-# hex_fc = config['hex_fc']
-# csv_folder = config['csv_folder']
-# hex_output = os.path.join(hex_output_folder, hex_gdb, hex_fc)
+#Adding fields to the hexagon feature classes to prepare for summarized data
+Start = time.time()
+# define working directory
+config = read_yaml_config()
+hex_root = config['root_folder']
+hex_output_folder = config['hex_output_folder']
+hex_gdb = config['hex_gdb']
+hex_fc = config['hex_fc']
+csv_folder = config['csv_folder']
+hex_output = os.path.join(hex_output_folder, hex_gdb, hex_fc)
 
 
 def multi_add_fields_treelist(output_folder, grid):
@@ -23,41 +23,53 @@ def multi_add_fields_treelist(output_folder, grid):
     output folder: directory for output gdb by 4km grids
     field_list: fields need to be added
     """
-    gdb_path = os.path.join(output_folder, 'HEX_' + grid + '.gdb')
-    out_fc = os.path.join(gdb_path, grid)
+    out_fc = os.path.join(output_folder, 'HEX_' + grid + '.gdb', grid)
     
-    print('start adding fields ', grid)
-    for sp_type in ['CON', 'DEC']:
-        arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(5)+"_"+str(6)+"_COUNT", "SHORT")
-        arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(5)+"_"+str(6)+"_DBH", 'FLOAT')
-        arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(5)+"_"+str(6)+"_SP", 'TEXT', field_length=30)
+    field_names = [f.name for f in arcpy.ListFields(out_fc)]
 
-        for i in range(6, 50, 2):
-            arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(i)+"_"+str(i+2)+"_COUNT", "SHORT")
-            arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(i)+"_"+str(i+2)+"_DBH", 'FLOAT')
-            arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(i)+"_"+str(i+2)+"_SP", 'TEXT', field_length=30)
+    # Check for presence
+    if 'CON_HT_5_6' in field_names:
+        print(f"{grid} Field created")
+    else:
+        print('start adding fields ', grid)
+        for sp_type in ['CON', 'DEC']:
+            arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(5)+"_"+str(6)+"_COUNT", "SHORT")
+            arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(5)+"_"+str(6)+"_DBH", 'FLOAT')
+            arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(5)+"_"+str(6)+"_SP", 'TEXT', field_length=30)
+
+            for i in range(6, 50, 2):
+                arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(i)+"_"+str(i+2)+"_COUNT", "SHORT")
+                arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(i)+"_"+str(i+2)+"_DBH", 'FLOAT')
+                arcpy.AddField_management(out_fc, sp_type + '_HT_'+str(i)+"_"+str(i+2)+"_SP", 'TEXT', field_length=30)
 
 
 ##############################################
 ######## define path
-output_folder = r'S:\1845\2\03_MappingAnalysisData\02_Data\06_Hexagon_Production\02_Process\hex_output\GRID'
-area = 'AREA_B'
+area = 'AREA_G'
 
 # get the list of grids to process
-multiprocess = r'S:\1845\2\03_MappingAnalysisData\02_Data\06_Hexagon_Production\02_Process\csv_output\MultiProcessing_files_input_' + area + '.csv'
+multiprocess = os.path.join(csv_folder, 'MultiProcessing_files_input_' + area + '.csv')
 grid_list = pd.read_csv(multiprocess).GRID.unique()
-# grid_list = grid_list[0:2]
 grid_list.sort()
-# grid = 'AA_2'
-args = [(output_folder, grid) for grid in grid_list]
 
 Start = time.time()
 
-if __name__ == '__main__':
-    with Pool(processes=8) as pool:
-        pool.starmap(multi_add_fields_treelist, args)
 
+###################3
+##### test function
+# grid = 'A16'
+# multi_add_fields_treelist(output_folder, grid)
+
+#############################
+#### multi process
+output_folder = r'S:\1845\5\03_MappingAnalysisData\02_Data\06_Hexagon_Production\02_Process\hex_output\GRID'
+args = [(output_folder, grid) for grid in grid_list]
+cores = 10
+
+if __name__ == '__main__':
+    with Pool(processes=cores) as pool:
+        pool.starmap(multi_add_fields_treelist, args)
 End = time.time()
 
-print(round((End - Start)/60, 2), ' mins to finish')
+print(f"it takes {round((End - Start)/60, 2)} mins to finish")
 
