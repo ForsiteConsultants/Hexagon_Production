b92be6416b779ec748eedc6dfcfcdb80c7c8a7ef
awuah-kwame <kawuah@forsite.ca>
2025-09-07 20:54:06 -0700

    add mlens-based models implementation

--- sripts/3003_c_MultiProcess_modeling_to_csv.py
+++ sripts/3003_c_MultiProcess_modeling_to_csv.py
@@ -22,6 +22,8 @@ import pickle
 import numpy as np
 import pandas as pd
 import math
+from math import sqrt
+# import altair as alt
 
 # data
 from sklearn import datasets
@@ -32,10 +34,25 @@ from sklearn.dummy import DummyRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.impute import SimpleImputer
-
+#mlens
+import collections
+import collections.abc
+collections.Sequence = collections.abc.Sequence #monkey patch for mlens
+from mlens.ensemble import SuperLearner
 # classifiers / models
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error
+# from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error
+# quick fix for processing computer 1:
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
+
+def root_mean_squared_error(y_true, y_pred):
+    """Return RMSE, equivalent to sklearn ≥1.4 helper."""
+    return mean_squared_error(y_true, y_pred, squared=False)
+
+def rmse(yreal, yhat):
+	return sqrt(mean_squared_error(yreal, yhat))
+
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler, RobustScaler
 
 # other
@@ -71,15 +88,10 @@ def read_yaml_config():
 	"""
 	Read yaml config and return dictionary of items
 	"""
-	yml_file = r'S:\1845\2\03_MappingAnalysisData\03_Scripts\05_HEXAGON_PRODUCTION\config_hex_B.yml'
-	# with open('./data/config/config.yaml', 'r') as file:
-	# 	config = yaml.safe_load(file)
-	# 	return config
+	yml_file = r'S:\1845\5\03_MappingAnalysisData\03_Scripts\06_HexProduction\config_hex_G.yml'
 	with open(yml_file, 'r') as file:
 		config = yaml.safe_load(file)
 		return config
-      
-Start = time.time()
 
 # original folder including ITI, some CSV output, model output
 # define working directory
@@ -89,31 +101,29 @@ hex_output_folder = config['hex_output_folder']
 hex_gdb = config['hex_gdb']
 hex_fc = config['hex_fc']
 hex_output = os.path.join(hex_output_folder, hex_gdb, hex_fc)
-area = 'AREA_B'
+area = 'AREA_G'
 hex_y = config['hex_y_variables']
 treelist_y = config['treelist_y_variables']
+compiled_grids_folder = config['compiled_grids_folder']
 csv_folder = config['csv_folder']
 plot_chars_folder = config['plot_chars_folder']
 boruta_output_folder = config['boruta_output_folder']
 model_output_folder = config['model_output_folder']
 
-multiprocess = r'S:\1845\2\03_MappingAnalysisData\02_Data\06_Hexagon_Production\02_Process\csv_output\MultiProcessing_files_input_' + area + '.csv'
-grid_list = pd.read_csv(multiprocess).GRID.unique()
-hexid = 'HEXID' ### need to check HEXID spelling !!!!!!!!!!!
-grid_list.sort()
-
-# grid_list = grid_list[:6]
-# grid_list = ['AA_2']
-# grid = 'AA_2'
-# print(grid)
 
 # merge model input data
-def hex_modeling_output(grid, csv_folder, plot_chars_folder, hexid='HEXID'):
-    iti_comp = pd.read_csv(os.path.join(csv_folder, grid, 'ITI_compile_' + grid + '.csv'), low_memory=False).set_index(hexid)
+def hex_modeling_output(grid, compiled_grids_folder, csv_folder, plot_chars_folder, hexid='HEXID'):
+    iti_comp = pd.read_csv(os.path.join(compiled_grids_folder, grid, 'ITI_compile_' + grid + '.csv'), low_memory=False).set_index(hexid)
     plot_chars = pd.read_csv(os.path.join(plot_chars_folder, grid + '_PlotChars_bil.csv'), low_memory=False).set_index(hexid)
     # climate = pd.read_csv(os.path.join(csv_folder, grid, grid + '_climate_data.csv'), low_memory=False).set_index(hexid)
     # df = iti_comp.join(plot_chars, how='inner').join(climate, how='inner')
     df = iti_comp.join(plot_chars, how='inner')
+
+    iti_ranme_map= {'TPMD': 'TPM_D',
+                    'GVPHD': 'GVPH_D',} 
+    for col in iti_ranme_map.keys():
+        if col in df.columns:
+            df.rename(columns={col: iti_ranme_map[col]}, inplace=True)
 
     # # check the number of rows of each dataframe
     # indices_diff1 = iti_comp.index.difference(plot_chars.index).tolist()
@@ -149,6 +159,7 @@ def hex_modeling_output(grid, csv_folder, plot_chars_folder, hexid='HEXID'):
 
 
     for source in [ 'Hex', 'treeList']:
+        print(f"    → processing source = {source!r}, grid = {grid!r}")
         df_combined = []
         if source == 'Hex':
             var_list = hex_y
@@ -158,7 +169,7 @@ def hex_modeling_output(grid, csv_folder, plot_chars_folder, hexid='HEXID'):
             model = var[5:]
             pred_var = 'PRED_' + model
             varx_df = pd.read_csv(os.path.join(boruta_output_folder, var + '_new_VARX.csv'))
-            varx_list = varx_df[varx_df['IO'] == 'X'].varName.tolist()
+            varx_list = varx_df[(varx_df['IO'] == 'X') & (~varx_df['varName'].str.startswith('G_'))].varName.tolist()
             if model in varx_list:
                 pass
             else:
@@ -171,8 +182,10 @@ def hex_modeling_output(grid, csv_folder, plot_chars_folder, hexid='HEXID'):
                     print(' data contains nan! check columns: ', grid, na_cols)
                     df_cln = df_cln.dropna(axis=0, how='any') # drop nan rows due to differences on input data
             except Exception as e:
-                print(grid, var, e)
+                print(f"❗  ERROR  grid={grid!r}  source={source!r}  var={var!r}  missing cols: {e}")
                 continue
+                # print(grid, var, e)
+                # continue
 
             model_info = pd.read_csv(os.path.join(model_output_folder, source, model + '_Model_Info.csv'))
             b0 = model_info.at[0, 'B0_ERROR']
@@ -187,7 +200,20 @@ def hex_modeling_output(grid, csv_folder, plot_chars_folder, hexid='HEXID'):
             else:
                 model_output = os.path.join(model_output_folder, source, model + '.sav')
 
-            loaded_model = pickle.load(open(model_output, 'rb'))
+            # Confirm modl file exist
+            if not os.path.exists(model_output):
+                raise FileNotFoundError(f"Model file does not exist: {model_output}")
+            if os.path.getsize(model_output) == 0:
+                raise ValueError(f"Model file is empty: {model_output}")
+            
+            try:
+                with open(model_output, 'rb') as f:
+                    loaded_model = pickle.load(f)
+            except Exception as e:
+                print(f"Failed to load model {model_output}")
+                raise e
+            
+            #loaded_model = pickle.load(open(model_output, 'rb'))
             columns = list(df_cln.columns.values)
             X_all = df_cln[columns]
             try:
@@ -206,23 +232,51 @@ def hex_modeling_output(grid, csv_folder, plot_chars_folder, hexid='HEXID'):
 
         df_final = pd.concat(df_combined, axis=1)
         if source == 'Hex':
-            df_con = df_final[['PRED_CON_SPH_GT_5m', 'PRED_CON_BA_HA', 'PRED_DEC_SPH_GT_5m','PRED_DEC_BA_HA']]
+            try:
+                df_con = df_final[
+                    ['PRED_CON_SPH_GT_5m', 'PRED_CON_BA_HA',
+                     'PRED_DEC_SPH_GT_5m', 'PRED_DEC_BA_HA']
+                ]
+            except KeyError as e:
+                print(f"❗  ERROR in grid '{grid}' for source '{source}': missing columns {e}")
+                raise
             df_con.columns = ['SPH_con', 'BPH_con', 'SPH_dec', 'BPH_dec']
         if source == 'treeList':
             df_final.columns = [i[5:] for i in list(df_final.columns.values)]
             df_final = df_final.drop(columns=['SPH_con', 'BPH_con', 'SPH_dec', 'BPH_dec'])
             df_final = df_final.join(df_con, how='inner')
 
-        df_final.to_csv(os.path.join(csv_folder, grid, grid + '_' + source + '_predicted_output.csv'))
+        print('writing csv for grid:', grid, source)
+
+        out_dir = os.path.join(csv_folder, grid)
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        df_final.to_csv(os.path.join(out_dir, grid + '_' + source + '_predicted_output_v5.csv'))
 
 
+###############
+# update path
+Start = time.time()
 
-args = [(grid, csv_folder, plot_chars_folder) for grid in grid_list]
+multiprocess = multiprocess = r'S:\1845\5\03_MappingAnalysisData\02_Data\06_Hexagon_Production\02_Process\csv_output\MultiProcessing_files_input_' + area + '.csv'
+grid_list = pd.read_csv(multiprocess).GRID.unique()
+hexid = 'HEXID' ### need to check HEXID spelling !!!!!!!!!!!
+grid_list.sort()
+
+########################
+##### test function
+# grid = 'A16'
+# hex_modeling_output(grid, compiled_grids_folder, csv_folder, plot_chars_folder)
+
+
+######################
+##### multi processing
+args = [(grid, compiled_grids_folder, csv_folder, plot_chars_folder) for grid in grid_list]
 if __name__ == '__main__':
-    with Pool(processes=6) as pool:
+    with Pool(processes=8) as pool:
         pool.starmap(hex_modeling_output, args)
 
-# hex_modeling_output('AA_2', csv_folder, plot_chars_folder)
 End = time.time()
 
 print(round((End - Start)/60, 2), ' mins to finish')
